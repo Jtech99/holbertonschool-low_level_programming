5cb483a1534d3ed7bacb9d93335996b489787236
Jtech99 <eugi202205@gmail.com>
2025-10-29 13:32:37 +0000

    omg

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -7,12 +7,12 @@
  */
 void print_numbers(void)
 {
-    int n = 0;
+	int n = 0;
 
-    while (n < 10)
-    {
-        _putchar(n + '0');
-        n++;
-    }
-    _putchar('\n');
+	while (n < 10)
+	{
+		_putchar(n + '0');
+		n++;
+	}
+	_putchar('\n');
 }
