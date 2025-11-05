11924d3114722976f704b541719e53c6f9089c7e
Jtech99 <eugi202205@gmail.com>
2025-11-05 14:51:55 +0000

    ok

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -8,7 +8,7 @@ int main(void)
     int num = 42;
 
     printf("Before: %d\n", num);
-    reset_to_98(&num);  // pass the address of num
+    reset_to_98(&num);  /* pass the address of num */
     printf("After: %d\n", num);
 
     return 0;
