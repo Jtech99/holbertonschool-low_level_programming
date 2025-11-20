34fd614768056c4458467cd98b4e48e540f16f3d
Jtech99 <eugi202205@gmail.com>
2025-11-20 00:06:34 +0000

    ok

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -2,18 +2,16 @@
 #include <stddef.h>
 
 /**
-* print_name - prints a name
-* @name: name of the person
-* @f: pointer to function which will print the name
-*
-* Return: nothing
-*/
-
+ * print_name - prints a name
+ * @name: name of the person
+ * @f: pointer to a function that prints a name
+ *
+ * Return: nothing
+ */
 void print_name(char *name, void (*f)(char *))
 {
 	if (name == NULL || f == NULL)
 		return;
-	/* f: variable qui contient l'adresse d'une fonction */
-	/* f(name) → va à cette adresse, exécute la fonction et lui passe name */
+
 	f(name);
 }
