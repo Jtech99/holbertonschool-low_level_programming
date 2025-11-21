8bc51a722917dde1f3aad176a7635b36d3644210
Jtech99 <eugi202205@gmail.com>
2025-11-21 21:30:59 +0000

    ok

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -1,42 +1,41 @@
-#include "3-calc.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include "3-calc.h"
 
 /**
- * main - performs simple arithmetic operations
- * @argc: number of arguments
- * @argv: array of arguments
+ * main - the main function
+ * @argc: number of argurments
+ * @argv: array of all arguments
  *
- * Return: 0 on success
+ * Return: Always 0.
  */
 int main(int argc, char *argv[])
 {
-    int num1, num2;
-    int (*op)(int, int);
+	int num1, num2, (*func)(int, int);
 
-    if (argc != 4)
-    {
-        printf("Error\n");
-        exit(98);
-    }
+	if (argc != 4)
+	{
+		puts("Error");
+		exit(98);
+	}
 
-    op = get_op_func(argv[2]);
-    if (op == NULL)
-    {
-        printf("Error\n");
-        exit(99);
-    }
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+	func = get_op_func(argv[2]);
 
-    num1 = atoi(argv[1]);
-    num2 = atoi(argv[3]);
+	if (func == NULL)
+	{
+		puts("Error");
+		exit(99);
+	}
 
-    if ((argv[2][0] == '/' || argv[2][0] == '%') && num2 == 0)
-    {
-        printf("Error\n");
-        exit(100);
-    }
+	if ((strcmp(argv[2], "/") == 0 || strcmp(argv[2], "%") == 0) && num2 == 0)
+	{
+		puts("Error");
+		exit(100);
+	}
 
-    printf("%d\n", op(num1, num2));
-
-    return (0);
+	printf("%d\n", func(num1, num2));
+	return (0);
 }
